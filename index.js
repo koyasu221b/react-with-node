913d0a1e00950ac57fbeb007ca995424258b8fab
Will Huang <will_huang@trend.com.tw>
2019-01-09 22:57:57 +0800

    Add passport.js for Oauth

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/key.js')
+
 const  app = express();
 
+passport.use(new GoogleStrategy(
+    {
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        challbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+        console.log(accessToken)
+    }
+));
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+// app.get('/', (req, res) => {
+//     res.send({ hi: 'there'});
+// });
+
+app.get('/auth/google',
+        passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT);
+
